4ce08cd3bdd993b64174ee569eb72a893589e0bf
Sultan2404 <sultannafis24@gmail.com>
2025-06-28 00:26:37 +0700

    fix: correct index.html for Vite production

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 3000
-  }
+  base: '/'
 })
