305b70beb325a1fee4d7ed731203053111016bbb
Sultan2404 <sultannafis24@gmail.com>
2025-06-28 01:22:05 +0700

    fix: set correct entry point to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 // src/App.tsx
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import ChatPage from './pages/ChatPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 
 function App() {
-  const isAuthenticated = !!localStorage.getItem('user') // langsung baca dari localStorage
+  const location = useLocation()
+  const isAuthenticated = !!localStorage.getItem('user')
 
   return (
-    <Routes>
+    <Routes key={location.pathname}>
       <Route
         path="/"
         element={<Navigate to={isAuthenticated ? '/chat' : '/login'} />}
