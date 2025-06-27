015b178a9b1513aee4ba0dbc4073479eac9177bf
Sultan2404 <sultannafis24@gmail.com>
2025-06-28 00:19:18 +0700

    fix: final deploy with correct index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -7,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* BrowserRouter HANYA di sini */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
